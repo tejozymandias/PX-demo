e61841dc754de80725834b6959bb94fa2d0b5701
Your Name <you@example.com>
2021-02-24 00:26:58 -0800

    fixedStyling

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,79 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+
+
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message--success", "form__message--error");
+    messageElement.classList.add(`form__message--${type}`);
+}
+
+function setInputError(inputElement, message) {
+    inputElement.classList.add("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+}
+
+function clearInputError(inputElement) {
+    inputElement.classList.remove("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+
+
+
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+        // Logic
+        const formInputs = document.getElementsByClassName('form__input');
+        const userId = formInputs[0].value;	
+        
+                // PX Code
+                //passing user and account objects:
+                aptrinsic("identify",
+                {
+                //User Fields
+                "id": userId, // Required for logged in app users
+                "email": userId        
+                },
+                {
+                //Account Fields
+                "id": "PersonalGithub", //Required
+                "name":"Tej Ozymandias Github",
+                "Program": "Platinum" // flat custom attributes
+                });
+                //PX Code
+
+        if((userId.indexOf("gainsight.com")>-1) && (formInputs[1].value.length > 3)) {
+
+		document.getElementsByClassName('container-login')[0].style.display = "none";
+		document.getElementById('page-wrapper').style.display = "block";
+        } else{
+            alert('Invalid User Name and Password');
+        }
+
+
+        setFormMessage(loginForm, "error", "Invalid username/password combination");
+    });
+
+    document.querySelectorAll(".form__input").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+                setInputError(inputElement, "Username must be at least 10 characters in length");
+            }
+        });
+
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+        });
+    });
+});
+
+
 (function($) {
 
 	var $window = $(window),
