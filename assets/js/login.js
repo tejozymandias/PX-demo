da4ce83c6743b496f66da457ad1c88ad6586bfd1
Your Name <you@example.com>
2021-03-12 05:27:14 -0800

    new pages

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,46 +1,27 @@
 
-
-function setFormMessage(formElement, type, message) {
-    const messageElement = formElement.querySelector(".form__message");
-
-    messageElement.textContent = message;
-    messageElement.classList.remove("form__message--success", "form__message--error");
-    messageElement.classList.add(`form__message--${type}`);
-}
-
-function setInputError(inputElement, message) {
-    inputElement.classList.add("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
-}
-
-function clearInputError(inputElement) {
-    inputElement.classList.remove("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
-}
-
-document.addEventListener("DOMContentLoaded", () => {
+    
+    
+    
     const loginForm = document.querySelector("#login");
-    const createAccountForm = document.querySelector("#createAccount");
-
-    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.add("form--hidden");
-        createAccountForm.classList.remove("form--hidden");
-    });
-
-    document.querySelector("#linkLogin").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.remove("form--hidden");
-        createAccountForm.classList.add("form--hidden");
-    });
-
-    loginForm.addEventListener("submit", e => {
-        e.preventDefault();
-        // Logic
+ 
+    loginForm.addEventListener("submit", e => {   
         const formInputs = document.getElementsByClassName('form__input');
         const userId = formInputs[0].value;
-        
-                // PX Code
+        const password = formInputs[1].value;
+ if((userId.indexOf("gainsight.com")>-1) && (password.length > 3)) {
+     console.log('test');
+    e.preventDefault();
+    // Logic
+
+
+           sessionStorage.setItem("username", userId);          
+            sessionStorage.setItem("password", password);
+            window.location.href = "index.html";
+            document.getElementById('logging').innerText = "Logout";
+            document.getElementById('nav').style.display = "block";
+
+
+                            // PX Code
                 //passing user and account objects:
                 aptrinsic("identify",
                 {
@@ -62,25 +43,98 @@ document.addEventListener("DOMContentLoaded", () => {
                 });
                 //PX Code
 
-        if((userId.indexOf("gainsight.com")>-1) && (formInputs[1].value.length > 3)) {
-            window.location.href = "main.html";
+
         } else{
             alert('Invalid User Name and Password');
         }
+  });
 
 
-        setFormMessage(loginForm, "error", "Invalid username/password combination");
-    });
 
-    document.querySelectorAll(".form__input").forEach(inputElement => {
-        inputElement.addEventListener("blur", e => {
-            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
-                setInputError(inputElement, "Username must be at least 10 characters in length");
-            }
-        });
 
-        inputElement.addEventListener("input", e => {
-            clearInputError(inputElement);
-        });
-    });
-});
+
+// (function(){
+//     //check session storage
+//     if( sessionStorage.length > 0) {
+//         document.getElementById('logging').innerText = "Logout";
+//     } else{
+//         document.getElementById('nav').style.display = "none";
+//     }})();
+    
+    
+//     document.getElementById('logging').addEventListener("click", e=>{
+//         sessionStorage.clear()
+//         document.getElementById('logging').innerText = "Login";
+//         document.getElementById('nav').style.display = "none";
+//     });
+    
+//     function setFormMessage(formElement, type, message) {
+//         const messageElement = formElement.querySelector(".form__message");
+    
+//         messageElement.textContent = message;
+//         messageElement.classList.remove("form__message--success", "form__message--error");
+//         messageElement.classList.add(`form__message--${type}`);
+//     }
+    
+//     function setInputError(inputElement, message) {
+//         inputElement.classList.add("form__input--error");
+//         inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+//     }
+    
+//     function clearInputError(inputElement) {
+//         inputElement.classList.remove("form__input--error");
+//         inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+//     }
+    
+//     document.addEventListener("DOMContentLoaded", () => {
+//         const loginForm = document.querySelector("#login");
+    
+    
+    
+//         loginForm.addEventListener("submit", e => {
+//             e.preventDefault();
+//             // Logic
+//             const formInputs = document.getElementsByClassName('form__input');
+//             const userId = formInputs[0].value;	
+            
+//                     // PX Code
+//                     //passing user and account objects:
+//                     aptrinsic("identify",
+//                     {
+//                     //User Fields
+//                     "id": userId, // Required for logged in app users
+//                     "email": userId        
+//                     },
+//                     {
+//                     //Account Fields
+//                     "id": "PersonalGithub", //Required
+//                     "name":"Tej Ozymandias Github",
+//                     "Program": "Platinum" // flat custom attributes
+//                     });
+//                     //PX Code
+    
+//             if((userId.indexOf("gainsight.com")>-1) && (formInputs[1].value.length > 3)) {
+    
+//             document.getElementsByClassName('container-login')[0].style.display = "none";
+//             document.getElementById('page-wrapper').style.display = "block";
+//             } else{
+//                 alert('Invalid User Name and Password');
+//             }
+    
+    
+//             setFormMessage(loginForm, "error", "Invalid username/password combination");
+//         });
+    
+//         document.querySelectorAll(".form__input").forEach(inputElement => {
+//             inputElement.addEventListener("blur", e => {
+//                 if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+//                     setInputError(inputElement, "Username must be at least 10 characters in length");
+//                 }
+//             });
+    
+//             inputElement.addEventListener("input", e => {
+//                 clearInputError(inputElement);
+//             });
+//         });
+//     });
+    
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,80 +2,24 @@
 	Minimaxing by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+*/ 
+//sessionStorage.clear()
 
-
-
-function setFormMessage(formElement, type, message) {
-    const messageElement = formElement.querySelector(".form__message");
-
-    messageElement.textContent = message;
-    messageElement.classList.remove("form__message--success", "form__message--error");
-    messageElement.classList.add(`form__message--${type}`);
-}
-
-function setInputError(inputElement, message) {
-    inputElement.classList.add("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
-}
-
-function clearInputError(inputElement) {
-    inputElement.classList.remove("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.querySelector("#login");
-
-
-
-    loginForm.addEventListener("submit", e => {
-        e.preventDefault();
-        // Logic
-        const formInputs = document.getElementsByClassName('form__input');
-        const userId = formInputs[0].value;	
-        
-                // PX Code
-                //passing user and account objects:
-                aptrinsic("identify",
-                {
-                //User Fields
-                "id": userId, // Required for logged in app users
-                "email": userId        
-                },
-                {
-                //Account Fields
-                "id": "PersonalGithub", //Required
-                "name":"Tej Ozymandias Github",
-                "Program": "Platinum" // flat custom attributes
-                });
-                //PX Code
-
-        if((userId.indexOf("gainsight.com")>-1) && (formInputs[1].value.length > 3)) {
-
-		document.getElementsByClassName('container-login')[0].style.display = "none";
-		document.getElementById('page-wrapper').style.display = "block";
-        } else{
-            alert('Invalid User Name and Password');
-        }
-
-
-        setFormMessage(loginForm, "error", "Invalid username/password combination");
-    });
-
-    document.querySelectorAll(".form__input").forEach(inputElement => {
-        inputElement.addEventListener("blur", e => {
-            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
-                setInputError(inputElement, "Username must be at least 10 characters in length");
-            }
+(function (){
+    const user = sessionStorage.getItem('username');
+    if(user){
+        document.getElementById('logging').innerText = "Logout";
+        document.getElementById('nav').style.display = "block";
+        document.getElementById('logging').addEventListener("click",(e)=>{
+            e.preventDefault();
+            sessionStorage.clear()
+            document.getElementById('logging').innerText = "Login";
+            document.getElementById('nav').style.display = "none";
         });
-
-        inputElement.addEventListener("input", e => {
-            clearInputError(inputElement);
-        });
-    });
-});
-
+    } else if(user === null) {
+        document.querySelector('#nav').style.display = "none";
+    }
+})();
 
 (function($) {
 
