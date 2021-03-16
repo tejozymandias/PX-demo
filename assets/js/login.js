8d0513f247e5b9913aa834eedbaea461f88f433e
tejozymandias <36965842+tejozymandias@users.noreply.github.com>
2021-03-16 22:28:24 +0530

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -16,32 +16,42 @@
 
            sessionStorage.setItem("username", userId);          
             sessionStorage.setItem("password", password);
+     //Run PX
+       (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-BDSBPZZISNM1-2");
+
+//passing user and account objects:
+(function(){
+    aptrinsic("identify",
+    {
+    //User Fields
+      "id": "unique-user-id", // Required for logged in app users
+      "email": "userEmail@address.com",
+      "firstName": "John",
+      "lastName": "Smith",
+      "signUpDate": 1522697426479, //unix time in ms
+      "plan" : "gold", //Custom attributes - please create those custom attributes in Aptrinsic via Account Settings to be tracked.
+      "price" : 95.5,
+      "userHash": "" // optional transient for HMAC identification
+    },
+    {
+    //Account Fields
+      "id":"IBM", //Required
+      "name":"International Business Machine",
+      "Program": "Platinum" // flat custom attributes
+   });
+})();
+     
+     
             window.location.href = "index.html";
             document.getElementById('logging').innerText = "Logout";
             document.getElementById('nav').style.display = "block";
 
 
-                            // PX Code
-                //passing user and account objects:
-                aptrinsic("identify",
-                {
-                //User Fields
-                "id": userId, // Required for logged in app users
-                "email": userId,
-                "firstName": "John",
-                "lastName": "Smith",
-                "signUpDate": 1522697426479, //unix time in ms
-                "plan" : "gold", //Custom attributes - please create those custom attributes in Aptrinsic via Account Settings to be tracked.
-                "price" : 95.5,
-                "userHash": "" // optional transient for HMAC identification
-                },
-                {
-                //Account Fields
-                "id": "PersonalGithub", //Required
-                "name":"Tej Ozymandias Github",
-                "Program": "Platinum" // flat custom attributes
-                });
-                //PX Code
+             
 
 
         } else{
@@ -53,88 +63,4 @@
 
 
 
-// (function(){
-//     //check session storage
-//     if( sessionStorage.length > 0) {
-//         document.getElementById('logging').innerText = "Logout";
-//     } else{
-//         document.getElementById('nav').style.display = "none";
-//     }})();
-    
-    
-//     document.getElementById('logging').addEventListener("click", e=>{
-//         sessionStorage.clear()
-//         document.getElementById('logging').innerText = "Login";
-//         document.getElementById('nav').style.display = "none";
-//     });
-    
-//     function setFormMessage(formElement, type, message) {
-//         const messageElement = formElement.querySelector(".form__message");
-    
-//         messageElement.textContent = message;
-//         messageElement.classList.remove("form__message--success", "form__message--error");
-//         messageElement.classList.add(`form__message--${type}`);
-//     }
-    
-//     function setInputError(inputElement, message) {
-//         inputElement.classList.add("form__input--error");
-//         inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
-//     }
-    
-//     function clearInputError(inputElement) {
-//         inputElement.classList.remove("form__input--error");
-//         inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
-//     }
-    
-//     document.addEventListener("DOMContentLoaded", () => {
-//         const loginForm = document.querySelector("#login");
-    
-    
-    
-//         loginForm.addEventListener("submit", e => {
-//             e.preventDefault();
-//             // Logic
-//             const formInputs = document.getElementsByClassName('form__input');
-//             const userId = formInputs[0].value;	
-            
-//                     // PX Code
-//                     //passing user and account objects:
-//                     aptrinsic("identify",
-//                     {
-//                     //User Fields
-//                     "id": userId, // Required for logged in app users
-//                     "email": userId        
-//                     },
-//                     {
-//                     //Account Fields
-//                     "id": "PersonalGithub", //Required
-//                     "name":"Tej Ozymandias Github",
-//                     "Program": "Platinum" // flat custom attributes
-//                     });
-//                     //PX Code
-    
-//             if((userId.indexOf("gainsight.com")>-1) && (formInputs[1].value.length > 3)) {
-    
-//             document.getElementsByClassName('container-login')[0].style.display = "none";
-//             document.getElementById('page-wrapper').style.display = "block";
-//             } else{
-//                 alert('Invalid User Name and Password');
-//             }
-    
-    
-//             setFormMessage(loginForm, "error", "Invalid username/password combination");
-//         });
-    
-//         document.querySelectorAll(".form__input").forEach(inputElement => {
-//             inputElement.addEventListener("blur", e => {
-//                 if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
-//                     setInputError(inputElement, "Username must be at least 10 characters in length");
-//                 }
-//             });
-    
-//             inputElement.addEventListener("input", e => {
-//                 clearInputError(inputElement);
-//             });
-//         });
-//     });
-    
+
