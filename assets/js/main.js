8309a68e446405241989d8a751f4a78a299521e5
tejozymandias <36965842+tejozymandias@users.noreply.github.com>
2021-06-04 13:54:51 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,11 @@
 //Run PX Tag
 
-  <script type="text/javascript">
+
   (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
   })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-DT38BLOGLNV8-2-3");
-</script>
-
 
 
 
