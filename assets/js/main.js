7e3bbc5b60e857ae7d293ac574229e1d8f65071c
tejozymandias <36965842+tejozymandias@users.noreply.github.com>
2021-08-03 01:33:11 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-6EJAJ3QDC2HE-2");
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP--2");
 
 
 
